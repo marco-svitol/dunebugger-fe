d92b8dee0d57d05f70b998fb7750ecbe6978fc2a
marco <marco.cambon@gmail.com>
2025-03-24 00:05:48 +0100

    bugfix

--- src/components/websocket.js
+++ src/components/websocket.js
@@ -5,7 +5,7 @@ export const startWebSocket = (wssUrl, setConnectionId, setIsOnline, handleIncom
   const webPubSubClient = new WebPubSubClient(wssUrl, { autoRejoinGroups: true });
 
   webPubSubClient.on("connected", (event) => {
-    console.log("Connected to Web PubSub with ID:", event.connectionId);
+    // console.log("Connected to Web PubSub with ID:", event.connectionId);
     setConnectionId(event.connectionId);
     sessionStorage.setItem("connectionId", event.connectionId);
     // setIsConnected(true);
@@ -14,7 +14,7 @@ export const startWebSocket = (wssUrl, setConnectionId, setIsOnline, handleIncom
   });
 
   webPubSubClient.on("disconnected", (event) => {
-    console.log("WebSocket disconnected:", event);
+    // console.log("WebSocket disconnected:", event);
     // setIsConnected(false);
     setIsOnline(false);
     setConnectionId(null);
@@ -22,7 +22,7 @@ export const startWebSocket = (wssUrl, setConnectionId, setIsOnline, handleIncom
   });
 
   webPubSubClient.on("group-message", (message) => {
-    console.log("Received message:", message);
+    // console.log("Received message:", message);
     handleIncomingMessage(message);
   });
 
@@ -34,7 +34,7 @@ export const startWebSocket = (wssUrl, setConnectionId, setIsOnline, handleIncom
 export const joinGroup = async (client, GROUP_NAME) => {
   try {
     await client.joinGroup(GROUP_NAME);
-    console.log(`Joined group: ${GROUP_NAME}`);
+    // console.log(`Joined group: ${GROUP_NAME}`);
   } catch (error) {
     console.error("Failed to join group:", error);
   }
